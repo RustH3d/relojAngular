bc289ea74e485151b525fbd3817062ec451d8cf3
RustH3d <humbertoa00327@gmail.com>
2024-10-30 22:36:09 -0400

    relojv1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, NgZone } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule, DatePipe } from '@angular/common';
+import { interval } from 'rxjs';
+import { startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  providers: [DatePipe]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  fecha: number = Date.now();
+  hora: any;
   title = 'relojAngular';
+
+  constructor(private cdr: ChangeDetectorRef, private ngZone: NgZone) {}
+
+  ngOnInit(): void {
+    this.mostrarHora();
+  }
+
+  mostrarHora() {
+    this.ngZone.runOutsideAngular(() => {
+      interval(1000).pipe(startWith(0)).subscribe(() => {
+        this.hora = new Date();
+        this.ngZone.run(() => {
+          this.cdr.detectChanges();
+        });
+      });
+    });
+  }
 }
+
+
